e7fc600887b10e64aab5e5ef79499f49f4cc7632
Eric Fithian <ef2413@columbia.edu>
2017-03-22 02:32:14 -0400

    Updated gems, edit page

--- mini-capstone/app/controllers/recipes_controller.rb
+++ mini-capstone/app/controllers/recipes_controller.rb
@@ -1,2 +1,40 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+    render 'index.html.erb'
+  end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    @recipe = Recipe.new(
+    title: params[:title],
+    chef: params[:chef],
+    ingredients: params[:ingredients],
+    directions: params[:directions],
+    prep_time: params[:prep_time],
+    price: params[:price]
+    )
+    @recipe.save
+    redirect_to "/recipes/#{@recipe.id}"
+  end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+    render 'show.html.erb'
+  end
+
+  def edit
+    render 'edit.html.erb'
+  end
+
+  def update
+    redirect_to "/recipes/#{@recipe.id}"
+  end
+
+  def destroy
+
+  end
 end
