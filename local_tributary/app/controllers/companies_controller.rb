4b0c57070117a6c02fdae0e69825102b9cd8a679
Eric Fithian <ef2413@columbia.edu>
2017-04-27 18:09:18 -0400

    Updated commit with new work

--- local_tributary/app/controllers/companies_controller.rb
+++ local_tributary/app/controllers/companies_controller.rb
@@ -1,18 +1,26 @@
 class CompaniesController < ApplicationController
+  def index
+  end
+
   def new
   end
 
   def create
     company = Company.new(
       name: params[:name],
+      address: params[:address],
+      image_description: params[:image_description],
+      company_description: params[:company_description],
+      phone_number: params[:phone_number],
+      deliver_radius: params[:deliver_radius],
+      website: params[:website],
+      total_locations: params[:total_locations],
       email: params[:email],
-      password: params[:password_digest],
-      password_confirmation: params[:password_confirmation]
-    )
+      password_digest: params[:password_digest]    )
     if company.save  
       session[:company_id] = company.id
       flash[:success] = 'You have successfully created an account!'
-      redirect_to "/recipes"
+      redirect_to "/companies"
     else
       render "new.html.erb"
     end
