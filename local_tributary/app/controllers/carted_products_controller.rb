d9bf949f4d3e8e486c2c268b714df52ce91b122a
Eric Fithian <ef2413@columbia.edu>
2017-05-03 20:46:00 -0400

    Added bootstrap, updated links, started carted products

--- local_tributary/app/controllers/carted_products_controller.rb
+++ local_tributary/app/controllers/carted_products_controller.rb
@@ -1,2 +1,20 @@
 class CartedProductsController < ApplicationController
+  before: authenticate_user! && authenticate_admin!
+
+  def index
+    @recipes = Product.all 
+  end
+
+  def create
+    @recipes = Product.find_by(id: params['product_id'])
+
+    @carted_products = CartedProduct.create(
+        quantity: params['quantity'],
+        user_id: current_user.id,
+        recipe_id: params[:product_id],
+        order_id: "carted"
+      )
+    flash[:sucess] = "You just successfully ordered!"
+    redirect_to "/carted_products"
+  end
 end
